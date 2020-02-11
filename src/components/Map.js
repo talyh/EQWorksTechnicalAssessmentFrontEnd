88d664eebce8b7dd8f9e8acd973c0054218a3d3b
talyh <taly.hotimsky@gmail.com>
2020-02-11 14:06:26 -0500

    Map recenters on first result

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,25 +3,32 @@ import styled from "styled-components";
 import GoogleMapReact from "google-map-react";
 import MapMarker from "./MapMarker";
 
-const Map = ({ data }) => (
-  <Container>
-    <GoogleMapReact
-      bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_KEY }}
-      defaultCenter={{ lat: 43.6426, lng: -79.3871 }}
-      defaultZoom={12}
-    >
-      {data.length > 0 &&
-        data.map(poi => (
-          <MapMarker
-            key={poi.poi_id}
-            lat={poi.lat}
-            lng={poi.lon}
-            text={poi.name}
-          />
-        ))}
-    </GoogleMapReact>
-  </Container>
-);
+const Map = ({ data }) => {
+  const defaultCenter = { lat: 43.6426, lng: -79.3871 };
+  const currentCenter =
+    data.length > 0 ? { lat: data[0].lat, lng: data[0].lon } : defaultCenter;
+
+  return (
+    <Container>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_KEY }}
+        defaultCenter={defaultCenter}
+        defaultZoom={15}
+        center={currentCenter}
+      >
+        {data.length > 0 &&
+          data.map(poi => (
+            <MapMarker
+              key={poi.poi_id}
+              lat={poi.lat}
+              lng={poi.lon}
+              text={poi.name}
+            />
+          ))}
+      </GoogleMapReact>
+    </Container>
+  );
+};
 
 const Container = styled.div`
   height: 100vh;
